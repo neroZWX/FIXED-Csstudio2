4cb6112db117c64633edf9768c7901c0f19e85ff
neroZWX <971370106@qq.com>
2019-11-10 05:12:56 +0000

    AddAttack

---
+++ CS-Studio2/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject AkBulletPrefab;
+    private Transform AKTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        AKTrans = transform.Find("Bip001/Bip001 Pelvis/Bip001 Spine/" +
+            "Bip001 R Clavicle/Bip001 R UpperArm/Bip001 R Forearm/Bip001 R Hand/WeaponContainer/weapon_fal");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Grounded")) {
+
+            if (Input.GetMouseButton(0)) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                bool IsCollider = Physics.Raycast(ray, out hit);
+                if (IsCollider) {
+                    Vector3 point = hit.point;
+                    anim.SetTrigger("Attack");  
+                    Shoot(point);
+                }
+            }
+        }
+    }
+    private void Shoot(Vector3 targetPoint) {
+        targetPoint.y = transform.position.y;
+        Vector3 dir = targetPoint - transform.position;
+        GameObject.Instantiate(AkBulletPrefab, AKTrans.position, Quaternion.LookRotation(dir));
+
+    }
+}
--- CS-Studio2/Assets/Scripts/Player/PlayerMove.cs
+++ CS-Studio2/Assets/Scripts/Player/PlayerMove.cs
@@ -15,13 +15,18 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Grounded")== false) return;
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        if (Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0)
+        {
+            transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime, Space.World);
 
-        transform.Translate(new Vector3(h, 0, v)* speed * Time.deltaTime,Space.World);
-        transform.rotation = Quaternion.LookRotation(new Vector3(h, 0, v));
-        //controllering the animation of the character
-        float res = Mathf.Max(Mathf.Abs(h), Mathf.Abs(v));
-        anim.SetFloat("Forward",res);
+            transform.rotation = Quaternion.LookRotation(new Vector3(h, 0, v));
+
+            //controllering the animation of the character
+            float res = Mathf.Max(Mathf.Abs(h), Mathf.Abs(v));
+            anim.SetFloat("Forward", res);
+        }
     }
 }
